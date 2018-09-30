9cacb848e28dba006556d9331f1a4f50f7cb618f
Kandarp Khandwala <kandarpck@gmail.com>
2018-09-30 03:59:27 -0400

    Enum for standard packet use

---
+++ labs/lab1/src/lab1_protocol/RIPPPacketType.py
@@ -0,0 +1,18 @@
+from enum import Enum, unique
+
+
+# 0 = SYN
+# 1 = ACK
+# 2 = SYN-ACK
+# 3 = DATA
+# 4 = FIN
+# 5 = FIN-ACK
+
+@unique
+class RIPPPacketType(Enum):
+    SYN = 0
+    ACK = 1
+    SYN_ACK = 2
+    DATA = 3
+    FIN = 4
+    FIN_ACK = 5
