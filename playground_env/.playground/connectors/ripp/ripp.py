e69e92f3f58b0e93ff45b8323b19147e28aa4a3f
Kandarp Khandwala <kandarpck@gmail.com>
2018-10-25 02:42:03 -0400

    Added and tested with the unreliable switch

--- playground_env/.playground/connectors/ripp/ripp.py
+++ playground_env/.playground/connectors/ripp/ripp.py
@@ -1,6 +1,6 @@
-from labs.lab1.src.lab1_protocol.RIPPClientProtocol import RIPPClientProtocol
-from labs.lab1.src.lab1_protocol.RIPPServerProtocol import RIPPServerProtocol
+from labs.lab1.src.lab1_protocol.RIPPClientProtocol import RippClientProtocol
+from labs.lab1.src.lab1_protocol.RIPPServerProtocol import RippServerProtocol
 from playground.network.common import StackingProtocolFactory
 
-pt_client = StackingProtocolFactory(lambda: RIPPClientProtocol())
-pt_server = StackingProtocolFactory(lambda: RIPPServerProtocol())
+pt_client = StackingProtocolFactory(lambda: RippClientProtocol())
+pt_server = StackingProtocolFactory(lambda: RippServerProtocol())
