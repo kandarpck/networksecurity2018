ab82c5bf22ec21a0fed8ede184ca649dde65b341
Kandarp Khandwala <kandarpck@gmail.com>
2018-11-24 02:35:56 -0500

    Derive all the ivs and read/write keys

--- labs/lab1/src/lab2_protocol/CipherUtil.py
+++ labs/lab1/src/lab2_protocol/CipherUtil.py
@@ -1,4 +1,5 @@
 from cryptography.hazmat.primitives.asymmetric.x25519 import X25519PrivateKey
+import hashlib
 
 
 class CipherUtils(object):
@@ -9,6 +10,18 @@ class CipherUtils(object):
 
     def generate_shared_key(self, priv_key, peer_pub_key):
         return priv_key.exchange(peer_pub_key)
+
+    def key_derivation(self, secret, hellos):
+        hasher = hashlib.sha256()
+        hasher.update(secret + hellos)
+        block_1 = hasher.digest()
+        hasher.update(block_1)
+        block_2 = hasher.digest()
+        iv_1 = block_1[:12]
+        iv_2 = block_1[12:24]
+        key_1 = block_2[:16]
+        key_2 = block_2[16:]
+        return iv_1, iv_2, key_1, key_2
 
 
 class ClientCipherUtils(CipherUtils):
@@ -21,6 +34,11 @@ class ClientCipherUtils(CipherUtils):
         self.shared_key = self.generate_shared_key(priv_key=self.private_key, peer_pub_key=peer_pub_key)
         return self.shared_key
 
+    def generate_client_keys(self, client_hello, server_hello):
+        client_iv, _, client_read, client_write = self.key_derivation(self.shared_key,
+                                                                      client_hello + server_hello)
+        return client_iv, client_read, client_write
+
 
 class ServerCipherUtils(CipherUtils):
     def __init__(self):
@@ -32,8 +50,15 @@ class ServerCipherUtils(CipherUtils):
         self.shared_key = self.generate_shared_key(priv_key=self.private_key, peer_pub_key=peer_pub_key)
         return self.shared_key
 
+    def generate_server_keys(self, client_hello, server_hello):
+        _, server_iv, server_write, server_read = self.key_derivation(self.shared_key,
+                                                                      client_hello + server_hello)
+        return server_iv, server_write, server_read
+
 
 if __name__ == '__main__':
     client = ClientCipherUtils()
     server = ServerCipherUtils()
     assert client.generate_client_shared(server.public_key) == server.generate_server_shared(client.public_key)
+    assert client.generate_client_keys(b'a', b'b')[1] == server.generate_server_keys(b'a', b'b')[1]
+    assert client.generate_client_keys(b'a', b'b')[2] == server.generate_server_keys(b'a', b'b')[2]
