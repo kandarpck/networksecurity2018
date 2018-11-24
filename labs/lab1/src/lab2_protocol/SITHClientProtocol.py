70d37767de3db73ed0cec81461f4aaab68cfd2c0
Kandarp Khandwala <kandarpck@gmail.com>
2018-11-23 22:37:33 -0500

    Client hello prepared using cipher and cert utilities

--- labs/lab1/src/lab2_protocol/SITHClientProtocol.py
+++ labs/lab1/src/lab2_protocol/SITHClientProtocol.py
@@ -1,10 +1,11 @@
-from logging import getLogger, WARNING, DEBUG
+import secrets
+from logging import getLogger, DEBUG
 
 from playground.network.common import StackingProtocol
 
-# from .PacketHandler import PacketHandler
-# from .RIPPPacket import RIPPPacket
-# from .RIPPPacketType import RIPPPacketType, StateType
+from .CertificateUtil import ClientCertificateUtils
+from .CipherUtil import ClientCipherUtils
+from .SITHPacket import SITHPacket
 from .SITHTransport import SithTransport
 
 logger = getLogger('playground.' + __name__)
@@ -17,13 +18,20 @@ class SithClientProtocol(StackingProtocol):
         self.ProtocolID = 'CLIENT'
         self.SithTransport = None
         self.transport = None
+        self.address = None
+        self.client_ciphers = ClientCipherUtils()
+        self.client_certs = ClientCertificateUtils(self.address)
 
     def connection_made(self, transport):
-        logger.debug('\n SITH Client connection made with {}\n'.format(transport.get_extra_info('peername')))
         self.transport = transport
         # Make connection
         logger.debug('\n SITH CLIENT MAKING CONNECTION \n')
         self.SithTransport = SithTransport(self)
+        client_hello = SITHPacket().sith_hello(random=secrets.randbits(256),
+                                               public_val=self.client_ciphers.public_key,
+                                               certs=[self.client_certs.client_cert,
+                                                      self.client_certs.intermediate_cert,
+                                                      self.client_certs.get_root_certificate()])
         self.higherProtocol().connection_made(self.SithTransport)
 
     def data_received(self, data):
