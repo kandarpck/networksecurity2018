9caf62f73a141819391d6267a66332ea01628a9a
Kandarp Khandwala <kandarpck@gmail.com>
2018-11-23 21:07:28 -0500

    SITH packet interfaces

--- labs/lab1/src/lab2_protocol/SITHPacket.py
+++ labs/lab1/src/lab2_protocol/SITHPacket.py
@@ -1,10 +1,8 @@
-import hashlib
-
 from playground.network.packet import PacketType
-from playground.network.packet.fieldtypes import BUFFER, STRING
+from playground.network.packet.fieldtypes import BUFFER, STRING, LIST
 from playground.network.packet.fieldtypes.attributes import Optional
 
-from .SITHPacketType import SITHPacketType, packet_type_mapping
+from .SITHPacketType import SITHPacketType
 
 
 class SITHPacket(PacketType):
@@ -16,8 +14,38 @@ class SITHPacket(PacketType):
         ("Type", STRING),  # HELLO, FINISH, DATA, CLOSE
         ("Random", BUFFER({Optional: True})),
         ("PublicValue", BUFFER({Optional: True})),
-        ("Certificate", BUFFER({Optional: True})),
+        ("Certificate", LIST(BUFFER)({Optional: True})),
         ("Signature", BUFFER({Optional: True})),
         ("Ciphertext", BUFFER({Optional: True}))
 
     ]
+
+    def sith_hello(self, random, public_val, certs):
+        hello = SITHPacket()
+        hello.Type = SITHPacketType.HELLO.value
+        hello.Random = random
+        hello.PublicValue = public_val
+        hello.Certificate = certs
+        return hello
+
+    def sith_finish(self, signature):
+        finish = SITHPacket()
+        finish.Type = SITHPacketType.FINISH.value
+        finish.Signature = signature
+        return finish
+
+    def sith_data(self, ciphertext):
+        data = SITHPacket()
+        data.Type = SITHPacketType.DATA.value
+        data.Ciphertext = ciphertext
+        return data
+
+    def sith_close(self, error=None):
+        close = SITHPacket()
+        close.Type = SITHPacketType.CLOSE.value
+        close.Ciphertext = error
+        return close
+
+
+if __name__ == '__main__':
+    pass
